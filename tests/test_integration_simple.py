00b50a6c6608dfe3d79a88b39e35c9b75bc12d21
Jan Tuitman <jan.tuitman@gmail.com>
2021-02-05 18:01:00 +0100

    add math example

--- tests/test_integration_simple.py
+++ tests/test_integration_simple.py
@@ -7,16 +7,23 @@ decision_table = [
   ("whitespace"     ,r'^(\s+)' ),
   ("numeric"        ,r'^(\d+)' ),                  
   ("word"           ,r'^([a-zA-Z]+)' ),                  
-  ("ignored"        ,r'^([^a-zA-Z \n\t]+)' )                
+  ("math"           ,r'^([\+\-\*\/=])' ),                  
+  ("ignored"        ,r'^([^a-zA-Z\+\-\*\/= \n\t]+)' )                
 ]
 
 main = CategoryTokenList()
 main.addCategory("whitespace",ReduceWhitespaceTokenizer())
 main.addCategory("numeric",NumericTokenizer())
 main.addCategory("word",LetterTokenizer())
+main.addCategory("math",CustomTokenList(["+","-","*","/","="]))
 main.addCategory("ignored",DropTokenizer())
 
-def test_integration1():
+def test_just_letters_and_numbers():
   tokens = main.encode(iterate_parts("The quick brown fox jumps over the lazy cow 21 times",decision_table))
   text = main.decode_tokens(tokens)
   assert "The quick brown fox jumps over the lazy cow 21 times" == text
+
+def test_just_a_sum():
+  tokens = main.encode(iterate_parts("10 + 7 = 17",decision_table))
+  text = main.decode_tokens(tokens)
+  assert "10 + 7 = 17" == text
